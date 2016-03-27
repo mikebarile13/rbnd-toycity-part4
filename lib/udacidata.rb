81b8fea3d889a97735ccbf59dfa445ce3c1be154
Mike Barile <mikebarile13@gmail.com>
2016-03-27 13:08:56 -0700

    Refactored code with new create_hash method.

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -24,6 +24,14 @@ class Udacidata
 		return data_object
 	end
 
+	def self.create_hash(key_arr, prod_arr)
+		data_object_hash = {}
+		key_arr.each_with_index do |value, index|
+			data_object_hash[value.to_sym] = prod_arr[index]
+		end
+		return data_object_hash
+	end
+
 	#Return array of all products 
 	def self.all
 		all_products = []
@@ -31,10 +39,7 @@ class Udacidata
 		headers = products.shift
 
 		products.each do |row|
-			data_object_hash = {}
-			headers.each_with_index do |value, index|
-				data_object_hash[value.to_sym] = row[index]
-			end
+			data_object_hash = self.create_hash(headers, row)
 			data_object = self.new(data_object_hash)
 			all_products << data_object
 		end
@@ -49,10 +54,7 @@ class Udacidata
 		n_products = []
 
 		products.each do |row|
-			data_object_hash = {}
-			headers.each_with_index do |value, index|
-				data_object_hash[value.to_sym] = row[index]
-			end
+			data_object_hash = self.create_hash(headers, row)
 			data_object = self.new(data_object_hash)
 			n_products << data_object
 		end
@@ -61,6 +63,45 @@ class Udacidata
 			return n_products[0]
 		else return n_products
 		end
+	end
+
+	#Returns last n products in database
+	def self.last(num_prods = 1)
+		headers = CSV.read(@@data_path)[0]
+		products = CSV.read(@@data_path).drop(1).reverse.first(num_prods)
+		n_products = []
+
+		products.each do |row|
+			data_object_hash = self.create_hash(headers, row)
+			data_object = self.new(data_object_hash)
+			n_products << data_object
+		end
+		
+		if n_products.length == 1
+			return n_products[0]
+		else return n_products
+		end
+	end
+
+	def find(loc)
+		headers = CSV.read(@@data_path)[0]
+		products = CSV.read(@@data_path).drop(1)
+		index = nil
+		count = 0
+
+		products.each do |row|
+			if row[0] == loc 
+				index = count
+			end
+			count += 1
+		end
+
+		data_object_hash = {}
+		headers.each_with_index do |value, index|
+			data_object_hash[value.to_sym] = row[index]
+		end
+		data_object = self.new(data_object_hash)
+
 	end
   
 end
