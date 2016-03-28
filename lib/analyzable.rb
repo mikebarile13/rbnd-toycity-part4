5ef0d2159cb0524317cdc6b381201ff15052ce23
Mike Barile <mikebarile13@gmail.com>
2016-03-27 17:48:33 -0700

    Completed analyzable methods except print report.

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -6,6 +6,43 @@ module Analyzable
   		total += product.price.to_f
   		count += 1
   	end
-  	return count
+  	return (total/count).round(2)
   end
+
+  def count_by_brand(arr)
+  	brands = []
+  	arr.each do |product|
+  		brands << product.brand
+  	end
+  	brands.uniq!
+
+  	brands = Hash[*brands.map {|k| [k, 0]}.flatten]
+
+  	arr.each do |product|
+  		brands[product.brand] += 1
+  	end
+
+  	return brands
+  end
+
+  def count_by_name(arr)
+  	names = []
+  	arr.each do |product|
+  		names << product.name
+  	end
+  	names.uniq!
+
+  	names = Hash[*names.map {|k| [k, 0]}.flatten]
+
+  	arr.each do |product|
+  		names[product.name] += 1
+  	end
+
+  	return names
+  end
+
+  def print_report(arr)
+
+  end
+
 end
