ae5936306d5221e1abae22def01507ca5e553678
Mike Barile <mikebarile13@gmail.com>
2016-03-31 14:57:05 -0700

    Updated find_by method do no longer require data path.

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -3,14 +3,10 @@ class Module
 	    attributes.each do |attribute|
 	    	new_method = %Q{
 			   def find_by_#{attribute}(data)
-			   		data_path = File.dirname(__FILE__) + "/data/data.csv"
-					products = CSV.read(data_path)
-					header = products[0]
-					products.shift
-					index = header.find_index("#{attribute}")
+					products = self.all			
 					products.each do |product|
-						if product[index] == data
-							data_object = self.clone(header, product)
+						if product.#{attribute} == data
+							data_object = product
 							return data_object
 						end
 					end
